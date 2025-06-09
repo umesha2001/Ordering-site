67a69fe1e30010438051e4377475c0cf5539a0a7
umesha2001 <umeshaudayangani2001@gmail.com>
2025-06-09 20:21:36 +0530

    change4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+      
+    </BrowserRouter>
+  </React.StrictMode>
 )
